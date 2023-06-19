b2b9ba32f682a1b78a0db96f765c09a0832b20a3
Luan Lazzari <luanlazzari@hotmail.com>
2023-06-18 23:41:38 -0300

    feat: project init with maven

---
+++ src/test/java/com/vsc/demo/VscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
